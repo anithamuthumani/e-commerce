efb47c5f8a9aa99cc3e4cfd204608e3a3ee5683e
Anitha <anithamuthumani22@gmail.com>
2025-02-25 16:08:05 +0530

    Add files via upload

---
+++ anitha_project/anitha_project/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for anitha_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path('products/create/', views.product_create, name='product_create'),
+    path('products/<int:pk>/update/', views.product_update, name='product_update'),
+    path('products/<int:pk>/delete/', views.product_delete, name='product_delete'),
+
+    path('', views.order_list, name='order_list'),
+    path('orders/<int:pk>/', views.order_detail, name='order_detail'),
+    path('orders/create/', views.order_create, name='order_create'),
+    path('orders/<int:pk>/update/', views.order_update, name='order_update'),
+    path('orders/<int:pk>/delete/', views.order_delete, name='order_delete'),
+    ]
---
+++ anitha_project/app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.customer_name
